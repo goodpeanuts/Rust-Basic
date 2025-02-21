23be5e851f15ebf0bbb2c878b2c3c2e134a6a9ff
zenghao.333 <zenghao.333@bytedance.com>
2025-02-21 12:18:38 +0800

    finish

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,18 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.entry(team_1_name.to_string()).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        scores.entry(team_2_name.to_string()).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        scores.get_mut(&team_1_name).unwrap().goals_scored += team_1_score;
+        scores.get_mut(&team_1_name).unwrap().goals_conceded += team_2_score;
+        scores.get_mut(&team_2_name).unwrap().goals_scored += team_2_score;
+        scores.get_mut(&team_2_name).unwrap().goals_conceded += team_1_score;
     }
     scores
 }
